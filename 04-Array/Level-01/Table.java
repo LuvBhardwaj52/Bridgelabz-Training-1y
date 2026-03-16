50eeec8185453846fa31300db066d0fd14efbc75
LuvBhardwaj52 <bhardwajluv52@gmail.com>
2026-03-16 10:53:30 +0530

    [luv Bhardwaj]commit

---
+++ 04-Array/Level-01/Table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+        int[] table = new int[10];
+        
+        for(int i = 0; i < 10; i++) {
+            table[i] = number * (i + 1);
+            System.out.println(number + " * " + (i+1) + " = " + table[i]);
+        }
+    }
+}
